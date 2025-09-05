53c7ed49d7d549e7e3ac73c94577cfaacafed6bc
Pedro Baleroni <pbaleroni@gmail.com>
2025-09-05 16:49:49 -0300

    remove println

--- src/main/java/classes/main.java
+++ src/main/java/classes/main.java
@@ -13,6 +13,5 @@ public class main {
     public static void main(String[] args) {
         //Pokemon(Numero, Nome, Tipo1, Tipo2?, vida, peso, altura, evolui)
         Pokemon novoPoke = new Pokemon(1, "Pikachu","Eletrico", 35, 6,40,true);
-        System.out.println(novoPoke.getNome());
     }
 }
